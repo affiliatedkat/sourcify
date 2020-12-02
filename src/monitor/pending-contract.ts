5cd7d8a1da2af019f7b9f7eea0a6961a01b7373d
FabijanC <fabijan.corak@gmail.com>
2020-12-02 16:09:49 +0100

    Enhance Repo and PendingContract

--- src/monitor/pending-contract.ts
+++ src/monitor/pending-contract.ts
@@ -1,27 +1,29 @@
-import { cborDecode } from '@ethereum-sourcify/core';
+import { CheckedContract, PathBuffer, StringMap } from '@ethereum-sourcify/core';
 import Repo from './repo';
-import Web3 from 'web3';
 import { Injector } from '@ethereum-sourcify/verification';
+import { ValidationService } from '@ethereum-sourcify/validation';
+import Logger from 'bunyan';
 
-type Source = {keccak256: string, urls: string[]};
-interface SourceMap {
-    [name: string]: Source;
+type SourceInfo = { keccak256: string, urls: string[] };
+interface SourceInfoMap {
+    [name: string]: SourceInfo;
 }
-type Metadata = {sources: SourceMap};
+type Metadata = { sources: SourceInfoMap };
 
 export default class PendingContract {
     private chain: string;
     private address: string;
-    private rawMetadata: string;
     private bytecode: string;
-    private pendingSources: SourceMap;
-    private fetchedSources: SourceMap;
-    private sourceRepo: Repo<Source>;
-    private metadataRepo: Repo<Metadata>;
+    private pendingSources: SourceInfoMap;
+    private fetchedSources: StringMap;
+    private sourceRepo: Repo;
+    private metadataRepo: Repo;
     private injector: Injector;
+    private logger = new Logger({ name: "Pending Contract" });
 
-    // TODO too many parameters
-    constructor(chain: string, address: string, bytecode: string, metadataHash: string, metadataRepo: Repo<Metadata>, sourceRepo: Repo<Source>, injector: Injector) {
+    // TODO too many parameters - switch to single object
+    // TODO injector - one instance per pending contract
+    constructor(chain: string, address: string, bytecode: string, metadataHash: string, metadataRepo: Repo, sourceRepo: Repo, injector: Injector) {
         this.chain = chain;
         this.address = address;
         this.bytecode = bytecode;
@@ -34,8 +36,7 @@ export default class PendingContract {
         this.metadataRepo.subscribe(metadataHash, this.addMetadata);
     }
 
-    private addMetadata(rawMetadata: string) {
-        this.rawMetadata = rawMetadata;
+    private addMetadata = (rawMetadata: string) => {
         const metadata: Metadata = JSON.parse(rawMetadata);
         this.pendingSources = metadata.sources;
         for (const name in this.pendingSources) {
@@ -44,7 +45,7 @@ export default class PendingContract {
         }
     }
 
-    private addFetchedSource(name: string, source: Source) {
+    private addFetchedSource = (source: string) => {
         const deleted = delete this.pendingSources[name];
 
         if (!deleted) {
@@ -58,12 +59,46 @@ export default class PendingContract {
     }
 
     private finalize(): void {
+        const pathBuffers: PathBuffer[] = [];
+        for (const sourceName in this.fetchedSources) {
+            const sourceValue = this.fetchedSources[sourceName];
+            pathBuffers.push({ path: sourceName, buffer: Buffer.from(sourceValue) });
+        }
+
+        const contracts = new ValidationService().checkFiles(pathBuffers);
+        this.checkContractArray(contracts);
+
         this.injector.inject({
             addresses: [this.address],
             chain: this.chain,
             bytecode: this.bytecode,
-            // files TODO
+            contracts
         });
+    }
+
+    private checkContractArray(contracts: CheckedContract[]) {
+        if (contracts.length !== 1) {
+            const msg = `Cannot inject more than one contract per chain address; ${contracts.length} attempted.`;
+            this.logger.error({
+                loc: "[PENDING_CONTRACT]",
+                chain: this.chain,
+                address: this.address
+            }, msg);
+            throw new Error(msg);
+        }
+
+        const contract = contracts[0];
+        if (!contract.isValid()) {
+            const msg = `Invalid contract ${contract.name} at chain ${this.chain} at address ${this.address}.`;
+            this.logger.error({
+                loc: "[PENDING_CONTRACT]",
+                name: contract.name,
+                chain: this.chain,
+                address: this.address,
+                missing: Object.keys(contract.missing)
+            }, msg);
+            throw new Error(msg);
+        }
     }
 }
 
--- src/monitor/repo.ts
+++ src/monitor/repo.ts
@@ -1,21 +1,39 @@
-export default class Repo<T> {
-    private address: string;
-    private refreshInterval: number;
-    private interval: NodeJS.Timeout;
-    private subscriptions: any; // TODO
+import Logger from "bunyan";
 
-    constructor(address: string, refreshInterval = 15) {
-        this.address = address;
-        this.refreshInterval = refreshInterval;
+type FileFetcher = (hash: string) => Promise<string>;
 
-        this.interval = setInterval(this.fetch.bind(this), refreshInterval * 1000);
+type FetchCallback= (fetchedFile: string) => any;
+
+declare interface Subscriptions {
+    [hash: string]: Array<FetchCallback>;
+}
+
+export default class Repo {
+    private fetcher: FileFetcher;
+    private subscriptions: Subscriptions;
+    private logger = new Logger({ name: "Repo" }); // TODO what kind of Repo?
+
+    constructor(fetcher: FileFetcher, refreshInterval = 15) {
+        this.fetcher = fetcher;
+        setInterval(this.fetch.bind(this), refreshInterval * 1000);
     }
 
     private fetch(): void {
-
+        for (const hash in this.subscriptions) {
+            this.fetcher(hash).then(file => {
+                this.notifySubscribers(hash, file);
+            }).catch(err => {
+                this.logger.error(err);
+            });
+        }
     }
 
-    subscribe(hash: string, callback: Function) {
+    private notifySubscribers(hash: string, file: string) {
+        const callbacks = this.subscriptions[hash];
+        callbacks.forEach(callback => callback(file));
+    }
+
+    subscribe(hash: string, callback: FetchCallback) {
         if (!(hash in this.subscriptions)) {
             this.subscriptions[hash] = [];
         }
